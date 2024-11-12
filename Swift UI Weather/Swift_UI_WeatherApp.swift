379777efbe0a8a7100929b7f24c95c63451f040a
Ahmed Nafie <Ahmedtareknafie@gmail.com>
2024-11-12 14:05:01 +0200

    Initial Commit

---
+++ Swift UI Weather/Swift_UI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UI_WeatherApp.swift
+//  Swift UI Weather
+//
+//  Created by Ahmed Nafie on 12/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
