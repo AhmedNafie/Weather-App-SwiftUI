d68c06cb23ce26eb00940dc5b47e7965cccc8866
Ahmed Nafie <Ahmedtareknafie@gmail.com>
2024-11-12 19:27:27 +0200

    Add main UI elements

--- Swift UI Weather/ContentView.swift
+++ Swift UI Weather/ContentView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(colors: [.blue,.white],
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+            .ignoresSafeArea()
+            VStack {
+                Text("Cairo, CA")
+                    .font(.system(size: 32,weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                
+                VStack {
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    Text("25 °")
+                        .font(.system(size: 70,weight: .medium))
+                        .foregroundColor(.white)
+
+                }
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
