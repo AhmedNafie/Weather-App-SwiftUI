2f28fbf8c1146d2170bc002df8c76ab91bd8d023
Ahmed Nafie <101005449+AhmedNafie@users.noreply.github.com>
2024-11-13 13:23:51 +0200

    Add HStack

--- Swift UI Weather/ContentView.swift
+++ Swift UI Weather/ContentView.swift
@@ -6,6 +6,23 @@
 //
 
 import SwiftUI
+struct Weather: Identifiable {
+    var id = UUID()
+    var day: String
+    var image: String
+    var temperature: Int
+}
+
+// Sample data to test
+let sampleWeatherData = [
+    Weather(day: "Tue", image: "sun.max.fill", temperature: 25 ),
+    Weather(day: "Wed", image: "cloud.fill", temperature: 18),
+    Weather(day: "Thu", image: "cloud.rain.fill", temperature: 15 ),
+    Weather(day: "Fri", image: "snow", temperature: -5 ),
+    Weather(day: "Sat", image: "sun.max.fill", temperature: 25 ),
+    Weather(day: "Sun", image: "cloud.fill", temperature: 18),
+    Weather(day: "Mon", image: "cloud.rain.fill", temperature: 15 ),
+]
 
 struct ContentView: View {
     var body: some View {
@@ -19,7 +36,6 @@ struct ContentView: View {
                     .font(.system(size: 32,weight: .medium, design: .default))
                     .foregroundColor(.white)
                     .padding()
-                
                 VStack {
                     Image(systemName: "cloud.sun.fill")
                         .renderingMode(.original)
@@ -29,7 +45,15 @@ struct ContentView: View {
                     Text("25 °")
                         .font(.system(size: 70,weight: .medium))
                         .foregroundColor(.white)
-
+                    Spacer()
+                        .frame(height: 20)
+                }
+                ScrollView(.horizontal, showsIndicators: false){
+                    HStack(spacing: 20) {
+                        ForEach(sampleWeatherData) { weather in
+                            DayView(day: weather.day, image: weather.image, temperature: weather.temperature)
+                        }
+                    }.padding(20)
                 }
                 Spacer()
             }
@@ -39,4 +63,26 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct DayView: View {
+    var day: String
+    var image: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack {
+            Text(day)
+                .font(.system(size: 25, weight: .medium))
+                .foregroundColor(.white)
+            Image(systemName: image)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50, height: 50)
+            Text("\(temperature)" + " °")
+                .font(.system(size: 25, weight: .medium))
+                .foregroundColor(.white)
+        }
+    }
 }
